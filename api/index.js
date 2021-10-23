a19ff6d0ac03b8e5661cbb2597d5cf525a268365
PaytonGibler01 <paytongibler2@gmail.com>
2021-10-23 13:01:45 -0400

    couldnt solve last 2 checks, kept getting error 404 instead of 500

---
+++ api/index.js
@@ -0,0 +1,126 @@
+// Build an apiRouter using express Router
+const express = require("express");
+const apiRouter = express.Router();
+const {
+  client,
+  createReport,
+  getOpenReports,
+  _getReport,
+  closeReport,
+  createReportComment,
+} = require("../db");
+// Import the database adapter functions from the db
+
+/**
+ * Set up a GET request for /reports
+ *
+ * - it should use an async function
+ *
+ * - it should await a call to getOpenReports
+ * - on success, it should send back an object like { reports: theReports }
+ * - on caught error, call next(error)
+ */
+
+apiRouter.get("/reports", async (req, res, next) => {
+  try {
+    const openReports = await getOpenReports();
+    const obj = { reports: []}
+    if (openReports) {
+       obj.reports =  openReports
+      res.send(obj);
+    } else {
+      next({
+        name: "error",
+        message: "openReports",
+      });
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+/**
+ * Set up a POST request for /reports
+ *
+ * - it should use an async function
+ * - it should await a call to createReport, passing in the fields from req.body
+ * - on success, it should send back the object returned by createReport
+ * - on caught error, call next(error)
+ */
+
+apiRouter.post("/reports", async (req, res, next) => {
+    try{
+        const report = await createReport(req.body);
+        if(report){
+            res.send(report);
+          } else{
+            next({
+              name: 'error',
+              message: 'createReport'
+            })
+          }
+      
+        } catch ({ name, message }) {
+          next({ name, message });
+        }
+});
+
+/**
+ * Set up a DELETE request for /reports/:reportId
+ *
+ * - it should use an async function
+ * - it should await a call to closeReport, passing in the reportId from req.params
+ *   and the password from req.body
+ * - on success, it should send back the object returned by closeReport
+ * - on caught error, call next(error)
+ */
+ apiRouter.delete("/reports/:reportId", async (req, res, next) => {
+     
+    try{
+        const close = await closeReport(req.params.reportId, req.body.password);
+        if(close){
+            res.send(close);
+          } else{
+            next({
+              name: 'error',
+              message: 'closeReport'
+            })
+          }
+      
+        } catch ({ name, message }) {
+          next({ name, message });
+        }
+});
+
+
+
+/**
+ * Set up a POST request for /reports/:reportId/comments
+ *
+ * - it should use an async function
+ * - it should await a call to createReportComment, passing in the reportId and
+ *   the fields from req.body
+ * - on success, it should send back the object returned by createReportComment
+ * - on caught error, call next(error)
+ */
+    apiRouter.post("/api/reports/:reportId/comments", async(req, res, next)=>{
+        const {reportId, fields} = req.body
+        try {
+            const comment = await createReportComment({reportId, fields});
+            // if(comment){
+            //     res.send(comment)
+            // } else{
+            //     next({
+            //         name: 'comment error',
+            //         message: 'problem with /api/reports/:id/comments'
+            //       })
+            // }
+            res.send(comment)
+        } catch (error) {
+            next(error)
+        }
+    })
+
+
+// Export the apiRouter
+module.exports = apiRouter;
